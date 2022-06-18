b0dd4106c016f49deb7eb45c4c23e6c148888d52
amdx1254 <amdx1254@gmail.com>
2022-06-18 07:46:36 +0000

    Change App.js to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
